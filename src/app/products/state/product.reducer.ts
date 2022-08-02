31ab6f996cbc864291df451b02f3457b90d06dbe
Soumyak1303 <soumyak1303@gmail.com>
2022-08-02 18:42:13 +0530

    seperating interfacing to accomodate lazy loading issues

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,16 +1,21 @@
 import { createAction, createReducer, on } from '@ngrx/store';
 import { Product } from '../product';
+import * as AppState from '../../state/app.state';
 
-export interface ProductState {
-    showProductCode: boolean;
-    currentProduct: Product;
-    products: Product;
+export interface State extends AppState.State {
+  products: ProductState;
 }
 
-export const productReducer = createReducer(
-  { showProductCode: true },
-  on(createAction('[Product] Toggle Product Code'), (state) => {
-   // console.log(`original state: ${JSON.stringify(state)}`);
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product;
+}
+
+export const productReducer = createReducer<ProductState>(
+  { showProductCode: true } as ProductState,
+  on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
+    // console.log(`original state: ${JSON.stringify(state)}`);
     return {
       ...state,
       showProductCode: !state.showProductCode,
