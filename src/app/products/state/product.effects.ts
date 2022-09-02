dda549297cd1066f764923911749f0628d1653dc
Soumyak1303 <soumyak1303@gmail.com>
2022-09-02 14:19:15 +0530

    dispatch update action and handle effect

--- src/app/products/state/product.effects.ts
+++ src/app/products/state/product.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, mergeMap } from 'rxjs/operators';
+import { catchError, concatMap, mergeMap, retry } from 'rxjs/operators';
 import { ProductService } from '../product.service';
 import * as ProductActions from './product.actions';
 import { map } from 'rxjs/operators';
@@ -21,6 +21,20 @@ export class ProductEffects {
           map((products) => ProductActions.loadProductsSuccess({ products })),
           catchError((error) =>
             of(ProductActions.loadProductsFailure({ error }))
+          )
+        )
+      )
+    );
+  });
+
+  updateProduct$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(ProductActions.updateProduct),
+      concatMap((action) =>
+        this.productService.updateProduct(action.product).pipe(
+          map((product) => ProductActions.updateProductSuccess({ product })),
+          catchError((error) =>
+            of(ProductActions.updateProductFailure({ error }))
           )
         )
       )
